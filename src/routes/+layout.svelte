3c58003ee1dc31faaafd39649ad5f16ac37bf065
nsdeschenes <nsdeschenes@outlook.com>
2022-10-13 23:09:33 -0300

    create very basic nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
+	import Link from '../components/Link.svelte';
 	import '../app.css';
 </script>
 
-<slot />
+<div class="container mx-auto">
+	<div class="flex flex-col items-center">
+		<nav class="flex flex-row gap-8">
+			<Link href="/">Home</Link>
+			<Link href="/about">About</Link>
+		</nav>
+		<slot />
+	</div>
+</div>
