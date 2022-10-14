f69c580e8e0d63f421b93939f9f4ba3d23f0ccdc
nsdeschenes <nsdeschenes@outlook.com>
2022-10-13 23:09:46 -0300

    add prerendered about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<div class="w-full">
+    <div class="flex flex-col">
+
+        <h1 class="text-3xl font-semibold">About</h1>
+        <p>Just a cool little test app trying out SvelteKit seeing how everything works</p>
+    </div>
+</div>
