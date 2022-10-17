ddc8332a392e90acc617105beac036decdae6453
nsdeschenes <nsdeschenes@outlook.com>
2022-10-17 10:45:53 -0300

    create pokemon list page

---
+++ src/routes/pokemons/[pokeId]/+page.server.ts
@@ -0,0 +1,22 @@
+import { ALL_MONS } from '../../../data/pokemon';
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = ({ params }) => {
+	const parsedId = parseInt(params.pokeId, 10);
+
+	if (parsedId <= 0 || parsedId > ALL_MONS.length) {
+		throw error(404, 'Pokemon ID out of range');
+	}
+
+	const mon = ALL_MONS[parsedId - 1];
+	const imgUrl = `/api/image/${mon}.png`;
+
+	return {
+		pokeId: parsedId,
+		pokeImgUrl: imgUrl,
+		pokeName: mon,
+    hasNext: parsedId < ALL_MONS.length,
+    hasPrev: parsedId > 1,
+	};
+};
