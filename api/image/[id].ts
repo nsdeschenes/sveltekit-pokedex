cfec041b62f7a45b79d9383af91b43e9b342ca8f
nsdeschenes <nsdeschenes@outlook.com>
2022-10-17 10:43:54 -0300

    add custom image api route

---
+++ api/image/[id].ts
@@ -0,0 +1,18 @@
+export async function get({ params }) {
+	const { id } = params;
+
+	const rawId = id.replace('.png', '');
+
+	const mon = parseInt(rawId, 10);
+	if (typeof mon !== 'number') return new Response('Not found', { status: 404 });
+
+	const data = await fetch(
+		`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${mon}.png`
+	);
+
+	const newRes = new Response(data.body);
+
+	newRes.headers.set('Cache-Control', 'max-age=31536000, immutable');
+
+	return newRes;
+}
