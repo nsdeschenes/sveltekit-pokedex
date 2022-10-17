6f551857faa37a4734a63b3b8c1a23a54a42072f
nsdeschenes <nsdeschenes@outlook.com>
2022-10-17 10:45:22 -0300

    start working on custom pokemon card

---
+++ src/components/PokemonCard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	interface Pokemon {
+		pokeName: string;
+		imageUrl: string;
+	}
+
+	export let pokemon: Pokemon;
+	export let index: number;
+</script>
+
+<a href={`pokemons/${index + 1}`} class="flex items-center hover:underline cursor-pointer">
+	<img src={pokemon.imageUrl} alt={pokemon.pokeName} style="image-rendering: pixelated;" />
+	<div>
+		Pokemon: {`${pokemon.pokeName.substr(0, 1).toUpperCase()}${pokemon.pokeName.substring(1)}`}
+	</div>
+</a>
