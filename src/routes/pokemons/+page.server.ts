91153fe069fa0b41bbca70fcfd70e7cb33170d4b
nsdeschenes <nsdeschenes@outlook.com>
2022-10-17 10:46:05 -0300

    create individual pokemon page

---
+++ src/routes/pokemons/+page.server.ts
@@ -0,0 +1,14 @@
+import { ALL_MONS } from "../../data/pokemon";
+import type {PageServerLoad} from './$types'
+
+export const load: PageServerLoad = () => {
+
+  const pokemonList = ALL_MONS.map((name, index) => ({
+    pokeName: name,
+    imageUrl: `/api/image/${index + 1}.png`,
+  }))
+
+  return {
+    pokemonList
+  }
+}
---
+++ src/routes/pokemons/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import PokemonCard from '../../components/PokemonCard.svelte';
+import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<div class="flex gap-4">
+	<ul>
+		{#each data.pokemonList as pokemon, index}
+			<li><PokemonCard pokemon={pokemon} index={index} /></li>
+		{/each}
+	</ul>
+</div>
